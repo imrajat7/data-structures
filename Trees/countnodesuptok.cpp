404b635adcc8e3a32419fcd20ac8373fbb30b5ba
imrajat7 <rajat110898@gmail.com>
2019-08-02 10:27:48 +0530

    Function to count nodes upto k added

---
+++ Trees/countnodesuptok.cpp
@@ -0,0 +1,35 @@
+// Following is the given Tree node structure
+/**************
+
+template <typename T>
+class TreeNode {
+public:
+    T data;
+    vector<TreeNode<T>*> children;
+    
+    TreeNode(T data) {
+        this->data = data;
+    }
+    
+    ~TreeNode() {
+        for (int i = 0; i < children.size(); i++) {
+            delete children[i];
+        }
+    }
+};
+***************/
+
+int nodesGreaterThanX(TreeNode<int> *root, int x) {
+    /* Don't write main().
+     * Don't read input, it is passed as function argument.
+     * Return output and don't print it.
+     * Taking input and printing output is handled automatically.
+     */
+    int num=0;
+    if(root->data>x)
+        num++;
+    for(int i=0;i<root->children.size();i++){
+        num += nodesGreaterThanX(root->children[i],x);
+    }
+    return num;
+}
